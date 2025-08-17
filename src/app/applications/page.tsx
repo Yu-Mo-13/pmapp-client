fd13afc5e06c135bd519ec27c8702d89f608cc68
Yuya Motozono <motozono.tech@gmail.com>
2025-08-17 15:14:24 +0900

    不要なコメント削除

--- src/app/applications/page.tsx
+++ src/app/applications/page.tsx
@@ -1,5 +1,3 @@
-// components/PmappUI.tsx
-
 import React from 'react';
 import { Application } from '@/api/services/application/applicationService';
 import ApplicationList from '@/app/applications/_components/ApplicationList';
