77b6a0b650e8a44837b366bfc0b10a0ccb6c067b
Yuya Motozono <motozono.tech@gmail.com>
2025-08-30 23:38:11 +0900

    ボタンの文言間違ってたので修正

--- src/app/applications/create/page.tsx
+++ src/app/applications/create/page.tsx
@@ -86,7 +86,7 @@ const ApplicationCreatePage: React.FC = () => {
           <button
             className="text-white w-36 px-6 py-3 rounded bg-[#3CB371] text-[18px] font-medium hover:opacity-80 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 transition-opacity duration-200"
           >
-            更新
+            登録
           </button>
         </div>
     </main>
