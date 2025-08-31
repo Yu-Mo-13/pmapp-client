8d35e939316edcc44627e5d04f5b39841fe1dce0
Yuya Motozono <motozono.tech@gmail.com>
2025-08-31 17:06:04 +0900

    applicationをオブジェクトに修正

--- src/app/applications/create/actions.ts
+++ src/app/applications/create/actions.ts
@@ -10,22 +10,18 @@ export interface FormState {
 
 export async function createApplication(prevState: FormState, formData: FormData): Promise<FormState> {
   const TOGGLE_ON = '1';
-  const name = formData.get('name') as string;
-  const accountClass = formData.get('account_class') === TOGGLE_ON;
-  const noticeClass = formData.get('notice_class') === TOGGLE_ON;
-  const markClass = formData.get('mark_class') === TOGGLE_ON;
-  const prePasswordSize = parseInt(formData.get('pre_password_size') as string);
+  const application = {
+    application: {
+      name: formData.get('name') as string,
+      account_class: formData.get('account_class') === TOGGLE_ON,
+      notice_class: formData.get('notice_class') === TOGGLE_ON,
+      mark_class: formData.get('mark_class') === TOGGLE_ON,
+      pre_password_size: parseInt(formData.get('pre_password_size') as string)
+    }
+  };
 
   try {
-    const response = await ApplicationService.create({
-      application: {
-        name,
-        account_class: accountClass,
-        notice_class: noticeClass,
-        mark_class: markClass,
-        pre_password_size: prePasswordSize,
-      }
-    });
+    const response = await ApplicationService.create(application);
 
     if ('errors' in response && response.errors) {
       return {
