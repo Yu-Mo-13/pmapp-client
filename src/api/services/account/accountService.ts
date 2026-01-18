bea43c44200cf3119310dd3d7e59650c709b4413
Yuya Motozono <motozono.tech@gmail.com>
2026-01-18 21:19:04 +0900

    feat: アカウント一覧API APIクライアント

---
+++ src/api/services/account/accountService.ts
@@ -0,0 +1,19 @@
+import apiClient from '../../client';
+import { ApiResponse } from '../../types';
+// import { extractValidationErrors } from '../../utils/validationErrorTransformer';
+
+export interface Account {
+  id: number;
+  name: string;
+  application_id: number;
+  application_name: string;
+  notice_class: boolean;
+}
+
+export type AccountIndexResponse = Account[];
+
+export class AccountService {
+  static async index(): Promise<ApiResponse<AccountIndexResponse>> {
+    return apiClient.get('/accounts');
+  }
+}
