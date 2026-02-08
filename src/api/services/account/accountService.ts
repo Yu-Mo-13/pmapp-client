cfaf4daf568570a00d39f39bc50f579762d6e306
Yuya Motozono <motozono.tech@gmail.com>
2026-02-08 17:42:31 +0900

    feat: APIクライアント(account show)

--- src/api/services/account/accountService.ts
+++ src/api/services/account/accountService.ts
@@ -1,5 +1,6 @@
 import apiClient from '../../client';
 import { ApiResponse } from '../../types';
+import { ApplicationShowResponse } from '../application/applicationService';
 // import { extractValidationErrors } from '../../utils/validationErrorTransformer';
 
 export interface Account {
@@ -14,8 +15,16 @@ export type AccountIndexResponse = {
   data: Account[];
 };
 
+export type AccountShowResponse = Account & {
+  application: ApplicationShowResponse;
+};
+
 export class AccountService {
   static async index(): Promise<ApiResponse<AccountIndexResponse>> {
     return apiClient.get('/accounts');
+  }
+
+  static async show(id: number): Promise<ApiResponse<AccountShowResponse>> {
+    return apiClient.get(`/accounts/${id}`);
   }
 }
